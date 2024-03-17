d17e26db47551d3b03f7e32aad84841306a5b745
Satya Susarla <satyasusarla@Satyas-MacBook-Air.local>
2024-03-17 12:00:09 +0530

    alsj

--- Practice/random practice/recursion.cpp
+++ Practice/random practice/recursion.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int maze(int n, int i, int j){
+    if(i==n-1 && j==n-1){
+        return 1;
+    }
+    if(i>=n || j>=n ){
+        return 0;
+    }
+    return maze(n,i+1,j)+maze(n,i,j+1);
+}
 int power(int n,int p){
     if(p==0){
         return 1;
@@ -32,10 +41,41 @@ void reversestring(char a[], int n){
  cout<<a[n-1];
  reversestring(a,n-1);
 }
+
+void prntsubs(string s, string ans){
+    if (s.length()==0){
+        cout<<ans<<endl;
+        return;
+    }
+     char ch = s[0];
+     string ros= s.substr(1);
+     prntsubs(ros, ans);
+     prntsubs(ros, ans+ch);
+}
+
+void prntprmutations(string s, string ans){
+    if(s.length()==0){
+        cout<<ans<<endl;
+        return;
+    }
+    for(int i=0;i<s.length();i++){
+        char ch=s[i];
+        string ros=s.substr(0,i)+s.substr(i+1);
+        prntprmutations(ros,ans+ch);
+    }
+
+}
+int zerooneknapsack(int value[], int wt[], int n, int w){
+    if(n==0 || w==0){
+        return 0;
+    }
+    if(wt[n-1]>w){
+       return zerooneknapsack(value, wt ,n-1,w);
+}
+    
+   return max(zerooneknapsack(value,wt, n-1, w-wt[n-1])+value[n-1], zerooneknapsack(value, wt ,n-1,w));
+}
 int main(){
-    cout<<power(4,3)<<endl;
-    cout<<factorial(4)<<endl;
-    cout<<fib(4)<<endl;
-    reversestring("binod",5);
+    cout<<maze(3,0,0)<<endl;
 return 0;
 }
